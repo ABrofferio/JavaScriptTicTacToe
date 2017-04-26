fcc3060e4e14bbeb870a7b0bf571af22b6fb96c7
Aja Brofferio <aja.brofferio@gmail.com>
2017-04-26 09:13:02 -0700

    additional win calculations and cats game declarations

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 $(document).ready(function() {
+    //arrays to hold the x and o coordinates to determine score
     let xChoices = [];
     let oChoices = [];
+    //array to hold all ids to determine a cat's game
+    let catsGame = [];
+    //assigning x and o attributes as well as visible html text based on click and double clicks
     $('.table, td').single_double_click(function() {
-    console.log($(this).attr('id'));
     let id = $(this).attr('id');
-    $(this).attr('value', 'X');
-    console.log($(this).attr('value'));
     document.getElementById(id).innerHTML = "X";
-    xChoices.push(id);
-    console.log(xChoices);
+    if (catsGame.indexOf(this.id)===-1){
+        catsGame.push(id);
+    }
+    //check if the id already exists in the array and if not push
+    if (xChoices.indexOf(this.id)===-1){
+        xChoices.push(id);
+    }
     if (xChoices.length >=3){
        calculateWin(xChoices, "X") 
-    }      
+    }
+    if (catsGame.length ===9 && "#winner".innerHTML!=='' ){
+        document.getElementById("winner").innerHTML= "Cat's Game";
+    }
 }, function() {
-    console.log($(this).attr('id'));
     let id = $(this).attr('id');
-    $(this).attr('value', 'O');
-    console.log($(this).attr('value'));
     document.getElementById(id).innerHTML = "O";
-    oChoices.push(id);
-    console.log(oChoices);
+    if (catsGame.indexOf(this.id)===-1){
+        catsGame.push(id);
+    }
+    //check if the id already exists in the array and if not push
+    if (oChoices.indexOf(this.id)===-1){
+        oChoices.push(id);
+    }
     if (oChoices.length >=3){
         calculateWin(oChoices, "O")
     }
-})    
+    if (catsGame.length ===9 && "#winner".innerHTML===''){
+        document.getElementById("winner").innerHTML= "Cat's Game";
+    }
+})
 });
 
 function ticTacToeGrid() {
@@ -43,12 +57,15 @@ function ticTacToeGrid() {
 }
 
 function calculateWin(choiceArray, winner){
-    match0 = 0;
-    match1 = 0;
-    match2 = 0;
-    match3 = 0;
-    match4 = 0;
-    match5 = 0;
+    let win = false;
+    let match0 = 0;
+    let match1 = 0;
+    let match2 = 0;
+    let match3 = 0;
+    let match4 = 0;
+    let match5 = 0;
+    let match6 = 0;
+    let match7 = 0;
     for (let i=0; i<choiceArray.length; i++){
         //using ternary operators to check the choice arrays for matches using regex to count the matches in rows and columns
         (match0<3 && /^0/.test(choiceArray[i]))? match0+=1: match0;
@@ -57,13 +74,13 @@ function calculateWin(choiceArray, winner){
         (match3<3 && /0$/.test(choiceArray[i]))? match3+=1: match3;
         (match4<3 && /1$/.test(choiceArray[i]))? match4+=1: match4;
         (match5<3 && /2$/.test(choiceArray[i]))? match5+=1: match5;
+        (choiceArray[i]==="0,0" || choiceArray[i]==="1,1" || choiceArray[i]==="2,2") ? match6 +=1:match6;
+        (choiceArray[i]==="0,2" || choiceArray[i]==="1,1" || choiceArray[i]==="2,0") ? match7 +=1:match7;
         //checking for 3 matches to declare a winner
-        if (match0===3 || match1===3 || match2===3 || match3===3 || match4===3 || match5===3){
+        if (match0===3 || match1===3 || match2===3 || match3===3 || match4===3 || match5===3 || match6===3 || match7===3){
             document.getElementById("winner").innerHTML= "Player "+winner+" Wins";
         }
     }
-    console.log(match0,match1,match2,match3,match4,match5)
 } 
 
 ticTacToeGrid();
-
